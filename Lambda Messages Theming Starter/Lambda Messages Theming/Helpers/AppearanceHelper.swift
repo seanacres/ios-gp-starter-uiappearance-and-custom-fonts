e526a42415b1c803f7c7ded3d204e72b9c27f608
seanacres <6416690+seanacres@users.noreply.github.com>
2019-07-15 12:16:14 -0400

    Added appearance helper file and updated to Swift 5

--- Lambda Messages Theming Starter/Lambda Messages Theming/Helpers/AppearanceHelper.swift
+++ Lambda Messages Theming Starter/Lambda Messages Theming/Helpers/AppearanceHelper.swift
@@ -2,8 +2,12 @@
 //  AppearanceHelper.swift
 //  Lambda Messages Theming
 //
-//  Created by Andrew R Madsen on 1/28/19.
+//  Created by Sean Acres on 7/15/19.
 //  Copyright © 2019 Lambda School. All rights reserved.
 //
 
 import Foundation
+
+enum AppearanceHelper {
+    
+}
